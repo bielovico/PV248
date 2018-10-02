5b14ae6ac1182631b42d275b76197350881f247a
bielovico <bielovico@gmail.com>
2018-10-02 23:57:49 +0200

    first commit

---
+++ 02-objects/scorelib.py
@@ -0,0 +1,150 @@
+import re
+
+re_composer = re.compile('Composer: (.*)')
+re_print_id = re.compile('Print Number: ([0-9]*)')
+re_edition = re.compile('Edition: (.*)')
+re_editor = re.compile('Editor: (.*)')
+re_partiture = re.compile('Partiture: (.*)')
+re_range = re.compile('([a-zA-Z]*--[a-zA-Z]*)')
+re_dates = re.compile('(\(\*?([0-9]{4})?-{0,2}\+?([0-9]{4})?\))')
+
+
+
+class Print:
+    print_id = None
+    edition = None
+    partiture = None
+    def __init__(self, edition, print_id, partiture):
+        self.edition = edition
+        self.print_id = print_id
+        self.partiture = partiture
+    def format(self):
+        pass
+    def composition(self):
+        return self.edition.composition
+
+class Edition:
+    composition = None
+    authors = []
+    name = None
+    def __init__(self, composition, authors, name):
+        self.composition = composition
+        self.authors = authors
+        self.name = name
+
+
+class Composition:
+    name = None
+    incipit = None
+    key = None
+    genre = None
+    year = None
+    voices = []
+    authors = []
+    def __init__(self, name, incipit, key, genre, year, voices, authors):
+        self.name = name
+        self.incipit = incipit
+        self.key = key
+        self.genre = genre
+        self.year = year
+        self.voices = voices
+        self.authors = authors
+    
+
+class Voice:
+    name = None
+    range = None
+    def __init__(self, name, range):
+        self.name = name
+        self.range = range
+
+class Person:
+    def __init__(self, name="", born=None, died=None):
+        self.name = name
+        self.born = born
+        self.died = died
+
+def parse_print(p):
+    ps = p.split('\n')
+    
+    print_id = ps[0].split(': ')[1]
+    
+    composers = ps[1].split(': ')[1]
+    composers = composers.split(';')
+    authors = []
+    for composer in composers:
+        dates = re_dates.match(composer)
+        if dates is not None:
+            born = dates.group(2)
+            died = dates.group(3)
+            name = re.sub(dates.group(1), '', composer)
+            person = Person(name, born, died)
+            authors.append(person)
+        else:
+            authors.append(Person(composer))
+
+    title = ps[2].split(': ')[1]
+
+    genres = ps[3].split(': ')[1]
+    # genres = genres.split(',')
+    
+    key = ps[4].split(': ')[1]
+    
+    c_year = ps[5].split(': ')[1]
+    
+    p_year = ps[6].split(': ')[1]
+    
+    edition_name = ps[7].split(': ')[1]
+    
+    editors = ps[8].split(': ')[1]
+    edit_authors = []
+    dates = re_dates.match(editors)
+    if dates is not None:
+        born = dates.group(2)
+        died = dates.group(3)
+        name = re.sub(dates.group(1), '', editors)
+        person = Person(name, born, died)
+        edit_authors.append(person)
+    else:
+        edit_authors.append(Person(editors))
+
+    partiture = ps[-2].split(': ')[1]
+    if partiture[:2] == 'no':
+        partiture = False
+    elif partiture[:3] == 'yes':
+        partiture = True
+    else:
+        partiture = None
+    
+    incipit = ps[-1].split(': ')[1]
+
+    voices = []
+    for line in ps[9:-1]:
+        if line[0] == 'V':
+            v = line[0].split(': ')[1]
+            range = re_range.match(v)
+            if range is not None:
+                range = range.group(1)
+                v = re.sub(range + ',? ?', '', v)
+                voice = Voice(v, range)
+                voices.append(voice)
+    
+    composition = Composition(title, incipit, key, genres, c_year, voices, authors)
+    edition = Edition(composition, edit_authors, edition_name)
+    pr = Print(edition, print_id, partiture)
+    return pr
+
+    
+
+
+    
+
+def load(filename):
+    f = open(filename, mode='rt', encoding='utf-8')
+    data = f.read()
+    f.close()
+    prints = data.split('\n\n')
+    pps = []
+    for p in prints:
+        pp = parse_print(p)
+        pps.append(pp)
