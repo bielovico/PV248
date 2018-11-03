23d99339e1b5ee6133e6cff95adf101f98d4c4d8
bielovico <bielovico@gmail.com>
2018-11-03 02:14:22 +0100

    added window generator and computation of the lowest highest seen peak

--- 06-FFT/peaks.py
+++ 06-FFT/peaks.py
@@ -3,26 +3,32 @@ import numpy as np
 
 frequency = 440  # Hz
 
-window_length = 0.1  # in seconds
-
+window_seconds = 1  # in seconds
 
 with wave.open('06-FFT\\sounds\\Sinus_{}Hz.wav'.format(frequency), mode='rb') as f:
     (nchannels, sample_width, framerate, nframes, _, _) = f.getparams()
     data = f.readframes(nframes)
 
-samples = []
-for integer in struct.iter_unpack('h', data):
-    samples.append(integer[0])
+window_frames = int(framerate * window_seconds)
 
-amplitudes = np.fft.rfft(samples)
-amplitudes = np.abs(amplitudes)
+def get_window(data):
+    counter = 0
+    window = []
+    for integer in struct.iter_unpack('h', data):
+        window.append(integer[0])
+        counter += 1
+        if counter == window_frames:
+            yield window
+            window = []
+            counter = 0
 
-peaks = np.argwhere(amplitudes >= 20*np.average(amplitudes))
+lowest = 24000
+highest = 0
+for window in get_window(data):
+    amplitudes = np.fft.rfft(window)
+    amplitudes = np.abs(amplitudes)
+    peaks = np.argwhere(amplitudes >= 20*np.average(amplitudes))
+    if peaks.min() < lowest:    lowest = peaks.min()
+    if peaks.max() > highest:   highest = peaks.max()
 
-
-print(peaks)
-
-
-
-
-
+print('low = {}, high = {}'.format(lowest, highest))
