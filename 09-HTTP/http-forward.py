c2b0bb96b4f8bb7b9c85cdfacc92db24d2dc4687
bielovico <bielovico@gmail.com>
2018-12-03 00:29:55 +0100

    changed POST processing in 09

--- 09-HTTP/http-forward.py
+++ 09-HTTP/http-forward.py
@@ -6,29 +6,39 @@ upstream = 'http://' + upstream + '/'
 
 import http.server
 import urllib.request as req
+from urllib.error import URLError
 import json
 
-timeout = 1
-
 class ForwarderHandler(http.server.BaseHTTPRequestHandler):
-    def do_GET(self):
-        request = req.Request(upstream, headers={'Accept-Encoding': 'identity', 'Accept-Charset': 'utf-8'})
-        resp = req.urlopen(request, timeout=timeout)
+    def write_forwarded(self, request, timeout=1):
+        try:
+            resp = req.urlopen(request, timeout=timeout)
+            code = resp.getcode()
+            headers = dict(resp.getheaders())
+            content = resp.read().decode('UTF-8')
+        except (http.server.socket.timeout, URLError):
+            code = 'timeout'
+            headers = {'Content-Type': 'application/json'}
+            content = ''
         self.send_response(200, 'OK')
-        for keyword, value in resp.getheaders():
+        for keyword, value in headers.items():
             self.send_header(keyword, value)
         self.end_headers()
-        output = {'code': resp.getcode(), 'headers': dict(resp.getheaders())}
-        content = resp.read().decode('UTF-8')
+        output = {'code': code, 'headers': headers}
         try:
-            json.loads(content)
-            output['json'] = content
+            j = json.loads(content)
+            output['json'] = j
         except ValueError:
             output['content'] = content
         self.wfile.write(bytes(json.dumps(output), encoding='UTF-8'))
 
+    def do_GET(self):
+        request = req.Request(upstream, headers={'Accept-Encoding': 'identity', 'Accept-Charset': 'utf-8'})
+        self.write_forwarded(request)
+
     def do_POST(self):
-        content = self.rfile.read().decode('utf-8')
+        content_length = int(self.headers['Content-Length'])
+        content = self.rfile.read(content_length).decode('utf-8')
         code = None
         try:
             params = json.loads(content)
@@ -43,37 +53,25 @@ class ForwarderHandler(http.server.BaseHTTPRequestHandler):
             code = 'invalid json'
         headers = params.get('headers')
         if headers is None:
-            headers = {'Content-Type': 'text/html; charset=UTF-8'}
+            headers = {'Content-Type': 'application/json'}
         content = params.get('content')
-        if (typ == 'POST') and (content is None):
-            code = 'invalid json'
+        if content is None:
+            if typ == 'POST':
+                code = 'invalid json'
+            else:
+                content = ''
         timeout = params.get('timeout')
         if timeout is None:
             timeout = 1
         if code is not None:
-            self.send_response(201, code)
-            self.send_header('Content-Type', 'text/html; charset=UTF-8')
+            self.send_response(200, 'OK')
+            self.send_header('Content-Type', 'application/json')
             self.end_headers()
             self.wfile.write(bytes(json.dumps({'code':code}), encoding='UTF-8'))
             return
         data = bytes(content, encoding='UTF-8')
         request = req.Request(url, headers=headers, method=typ, data=data)
-        resp = req.urlopen(request, timeout=timeout)
-        self.send_response(200, 'OK')
-        for keyword, value in resp.getheaders():
-            self.send_header(keyword, value)
-        self.end_headers()
-        output = {'code': resp.getcode(), 'headers': dict(resp.getheaders())}
-        content = resp.read().decode('UTF-8')
-        try:
-            json.loads(content)
-            output['json'] = content
-        except ValueError:
-            output['content'] = content
-        self.wfile.write(bytes(json.dumps(output), encoding='UTF-8'))
-
-class ForwarderServer(http.server.HTTPServer):
-    pass
+        self.write_forwarded(request, timeout)
 
 def run(server_class=http.server.HTTPServer, handler_class=http.server.BaseHTTPRequestHandler):
     server_address = ('', port)
